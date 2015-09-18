21fb2baa5078a908e03434dc6d195408e79d8e96
Martin Hlaváč <info@mhlavac.net>
2015-09-18 22:50:10 +0200

    Add echoes to make sure user can see what the password is needed for

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+echo  "apt-get install zsh curl git wget"
 sudo apt-get -y install zsh curl git wget && \
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > antigen.zsh && \
+echo  "install zsh antigen"
 sudo mv antigen.zsh /opt && \
 wget -qO- https://raw.githubusercontent.com/mhlavac/my-zsh/master/.zshrc > ~/.zshrc && \
+echo  "Password required for changing shell to /bin/zsh"
 chsh -s /bin/zsh && \
 zsh
