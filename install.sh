1cd886266327b800d5093baecf7192736bf7f8c9
Martin Hlaváč <info@mhlavac.net>
2015-09-18 22:46:05 +0200

    Don't continue if something fails + install without asking

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sudo apt-get install zsh curl git wget
-curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > antigen.zsh
-sudo mv antigen.zsh /opt
-wget -qO- https://raw.githubusercontent.com/mhlavac/my-zsh/master/.zshrc > ~/.zshrc
-chsh -s /bin/zsh
+sudo apt-get -y install zsh curl git wget && \
+curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > antigen.zsh && \
+sudo mv antigen.zsh /opt && \
+wget -qO- https://raw.githubusercontent.com/mhlavac/my-zsh/master/.zshrc > ~/.zshrc && \
+chsh -s /bin/zsh && \
 zsh
