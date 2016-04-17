e95765018096df875c34eabc1db8f18e0f7f2f32
Martin Hlaváč <info@mhlavac.net>
2016-04-17 21:27:24 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,5 +13,5 @@ cd git_fresh && sudo ./install && cd ../ && \
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions && \
 
 # Set default shell to zsh
-chsh -s /bin/zsh && \
+sudo chsh -s /bin/zsh $USER && \
 zsh
